4532692422eaeed57e7bd5b411a722ffcbf85141
Wilian Natan Moraes Queiroz <wnatanmq@gmail.com>
2023-11-04 11:29:39 -0300

    Initial commit, added one route and added slf4 for simple logging.

---
+++ MineralObjectDetector/src/main/java/org/root/MineralObjectDetector/MineralObjectDetectorApplication.java
@@ -0,0 +1,13 @@
+package org.root.MineralObjectDetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MineralObjectDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineralObjectDetectorApplication.class, args);
+    }
+}
---
+++ MineralObjectDetector/src/main/java/org/root/MineralObjectDetector/controller/MinerController.java
@@ -0,0 +1,20 @@
+package org.root.MineralObjectDetector.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/miners")
+public class MinerController {
+
+    private final Logger logger = LoggerFactory.getLogger(MinerController.class);
+
+    @RequestMapping(value = "current", method = RequestMethod.GET)
+    public void Current(){
+        logger.info("Current called!");
+    }
+}
